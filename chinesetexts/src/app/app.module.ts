815ff1edfa7d86fba426a0c6addc2d973b76575c
José Víctor García Llorente <128757328+josevictorgarcia@users.noreply.github.com>
2025-07-06 01:37:37 +0200

    Add ngx-translate support for English and Spanish; translate header and footer components.

--- chinesetexts/src/app/app.module.ts
+++ chinesetexts/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { AppHeader } from './components/header.component';
 import { AppFooter } from './components/footer.component';
 import { AppShowText } from './components/show-text.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TextPage } from './components/text-page.component';
 import { TextForm } from './components/text-form.component';
 import { Flashcards } from './components/flashcards.component';
 import { FlashcardPage } from './components/flashcard.component';
 import { UserForm } from './components/user-form.component';
 import { Profile } from './components/profile.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -33,7 +39,14 @@ import { Profile } from './components/profile.component';
     BrowserModule,
     routing,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- chinesetexts/src/app/components/header.component.ts
+++ chinesetexts/src/app/components/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../sevices/loginService';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'header',
@@ -16,10 +17,18 @@ export class AppHeader {
     @ViewChild('loginErrorModal', { static: true })
     public loginErrorModal: TemplateRef<void> | undefined;
     
+    //Con configurar el traductor en solo un componente es suficiente. Pero hay que tener en cuenta que ese componente tiene que estar todo el rato en pantalla (para que el traductor esté "presente" en todo momento). Por eso lo ponemos en el header, porque el header siempre se va a encontrar en todas las páginas de la aplicación web.
     constructor(
       public loginService: LoginService, // Cambiado a público para acceder desde la plantilla
-      private router: Router
-    ) {}
+      private router: Router,
+      private translate: TranslateService
+    ) {
+      this.translate.setDefaultLang('en'); // Establecer el idioma por defecto
+    }
+
+    public changeLanguage(language: string) {
+      this.translate.use(language); // Cambiar el idioma
+    }
 
     public login() {
       if (this.loginEmail && this.loginPassword) {
@@ -33,10 +42,10 @@ export class AppHeader {
           //this.modalService.open(this.loginErrorModal, { centered: true });
           this.loginEmail = '';
           this.loginPassword = '';
-          this.messageError = 'Credenciales incorrectas. Por favor, intenta de nuevo.'
+          this.messageError = '"Incorrect credentials. Please try again."'
         });
       } else {
-        this.messageError = 'Por favor, completa todos los campos.';
+        this.messageError = 'Please fill in all fields.';
       }
     }
 
